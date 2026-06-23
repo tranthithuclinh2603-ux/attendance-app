e1948110d285fbe36378eb20091019697ebe082f
tranthithuclinh2603-ux <tranthithuclinh2603@gmail.com>
2026-06-23 20:20:50 +0700

    fix cors for vercel

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+app.use(cors({ origin: ['http://localhost:3000', 'https://attendance-app-eight-sigma.vercel.app'], credentials: true }));
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
